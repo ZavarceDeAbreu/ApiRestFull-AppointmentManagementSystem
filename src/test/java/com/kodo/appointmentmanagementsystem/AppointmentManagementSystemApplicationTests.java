11d76c0bb45cad4c0a08af49d1bd23c79fb2b92a
Dayanna Zavarce <natali.zavarcer@gmail.com>
2022-08-27 20:41:28 -0300

    project configuration

---
+++ src/test/java/com/kodo/appointmentmanagementsystem/AppointmentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodo.appointmentmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
